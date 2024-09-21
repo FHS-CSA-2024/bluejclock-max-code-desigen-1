d8dba80d2ecff8873a48289301fefbb5d32cea93
Adarsh F <77176659+max-code-desigen@users.noreply.github.com>
2024-09-20 20:27:27 -0700

    Update ClockDisplay12Hour.java
    FINALLY!

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -1,6 +1,63 @@
+package src.main.java;
+
 
 public class ClockDisplay12Hour
 {
     //Extend ClockDisplay for a 12 hour clock. This means you need to track AM/PM
     //Update all methods and parameters to accomodate this change
+    
+    private NumberDisplay hourDisplay;
+    private NumberDisplay minuteDisplay;
+    private String formattedTime;
+    private String periodIndicator;
+
+    public ClockDisplay12Hour() {
+        hourDisplay = new NumberDisplay(24);
+        minuteDisplay = new NumberDisplay(60);
+        periodIndicator = "AM";
+        updateTimeDisplay();
+    }
+
+    public ClockDisplay12Hour(int hours, int minutes, String amPm) {
+        hourDisplay = new NumberDisplay(24);
+        minuteDisplay = new NumberDisplay(60);
+        setClockTime(hours, minutes, amPm);
+        updateTimeDisplay();
+    }
+
+    public void advanceTime() {
+        minuteDisplay.increment();
+        if (minuteDisplay.getValue() == 0) {
+            hourDisplay.increment();
+        }
+        if (hourDisplay.getValue() == 12 && minuteDisplay.getValue() == 0) {
+            togglePeriod();
+        }
+        updateTimeDisplay();
+    }
+
+    private void togglePeriod() {
+        if (periodIndicator.equals("AM")) {
+            periodIndicator = "PM";
+        } else {
+            periodIndicator = "AM";
+        }
+    }
+
+    public void setClockTime(int hours, int minutes, String amPm) {
+        periodIndicator = amPm;
+        minuteDisplay.setValue(minutes);
+        hourDisplay.setValue(hours);
+        updateTimeDisplay();
+    }
+
+    public String getTime() {
+        return formattedTime;
+    }
+
+    private void updateTimeDisplay() {
+        String timeString = hourDisplay.getDisplayValue() + ":" + minuteDisplay.getDisplayValue();
+        formattedTime = timeString + periodIndicator;
+    }
 }
+
